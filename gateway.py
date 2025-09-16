e76c74ff7530522e7c8eb601722eed736be11e40
João Raphael <joaoraph@gmail.com>
2025-09-16 16:49:27 -0300

    feat: conexao do gateway a cada endpoint padrao do servico

--- gateway.py
+++ gateway.py
@@ -6,11 +6,41 @@ app = Flask(__name__)
 def home():
     return "Gateway funcionando"
 
+
+@app.route('/gateway/cpar', methods=['GET'])
+def cpar():
+    response = requests.get('http://127.0.0.1:5001/cpar')
+    return response.json()
+
 @app.route('/gateway/cursos', methods=['GET'])
 def cursos():
-    response = request
-    .get('http://127.0.0.1:5001/cursos')
+    response = requests.get('http://127.0.0.1:5002/cursos')
      return response.json()
+
+@app.route('/gateway/chatbot', methods=['GET'])
+def chatbot():
+    response = requests.get('http://127.0.0.1:5003/chatbot')
+    return response.json()
+
+@app.route('/gateway/identidade', methods=['GET'])
+def identidade():
+    response = requests.get('http://127.0.0.1:5004/identidade')
+    return response.json()
+
+@app.route('/gateway/insights', methods=['GET'])
+def insights():
+    response = requests.get('http://127.0.0.1:5005/insights')
+    return response.json()
+
+@app.route('/gateway/notificacoes', methods=['GET'])
+def notificacoes():
+    response = requests.get('http://127.0.0.1:5006/notificacoes')
+    return response.json()
+
+@app.route('/gateway/pagamentos', methods=['GET'])
+def pagamentos():
+    response = requests.get('http://127.0.0.1:5007/pagamentos')
+    return response.json()
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
