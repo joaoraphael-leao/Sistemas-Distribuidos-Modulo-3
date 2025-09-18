d4f382b8203018a3f9151631ea86dbf52196fcdc
Joao Raphael Leão <142404597+joaoraphael-leao@users.noreply.github.com>
2025-09-18 15:21:15 -0300

    conexao entre cursos e chatbot feita

--- routes/cursos.py
+++ routes/cursos.py
@@ -9,7 +9,7 @@ port = 5001
 @app.route('/cursos', methods=['GET'])
 def cursos():
     message = "Servico de Cursos conectado -> Endpoint GET /cursos"
-        return jsonify(message)
+    return jsonify(message)
         
 @app.route('/cursos/ver_inscricoes_do_usuario/<id_usuario>', methods=['GET'])
 def ver_inscricoes_do_usuario(id_usuario):
@@ -25,6 +25,31 @@ def ver_inscricoes_do_usuario(id_usuario):
         ]
     }
     return jsonify(message)
+
+@app.route("/cursos/consumir_midia_curso/<id_curso>")
+def consumir_midia_curso(id_curso):
+    requisicao_servico_midia_conteudo = requests.get("http://localhost:5008/midia_conteudo/" + id_curso).json()
+    midia_curso = requisicao_servico_midia_conteudo["midia_curso"]
+    return jsonify(midia_curso)
+
+@app.route("/cursos/tirar_duvida/<id_curso>", methods=['GET'])
+def tirar_duvida(id_curso):
+
+    # id_curso já está disponível como parâmetro da função
+
+    aula_contexto = "Aula de Python Condicionais"
+    duvida = "Como fazer um if else em Python?"
+
+    body = {
+        'aula_contexto': aula_contexto,
+        'duvida': duvida
+    }
+    
+    response = requests.post("http://localhost:5003/chatbot/duvida", json=body)
+
+    resposta_duvida = response.json()
+    print(resposta_duvida)
+    return jsonify(resposta_duvida)
 
 
 if __name__ == '__main__':
--- routes/midia_conteudo.py
+++ routes/midia_conteudo.py
@@ -11,5 +11,19 @@ def midia_conteudo():
     message = "Servico de Midia e Conteudo Conectado -> Endpoint GET /midia_conteudo"
     return jsonify(message)
 
+@app.route('/midia_conteudo/<id_curso>', methods=['GET'])
+def midia_conteudo_curso():
+    response = {
+        "message": "Servico de Midia e Conteudo Conectado -> Endpoint GET /midia_conteudo/" + id_curso,
+        "midia_curso": {
+            "id": id_curso,
+            "dados_midia": {
+                "nome": "Midia 1",
+                "descricao": "Descricao da Midia 1",
+            },
+        }
+    }
+    return jsonify(response)
+
 if __name__ == '__main__':
     app.run(debug=True, port=port)
